72a02db4802dc54119a7491df0c1895063491e40
Arseniy <krutoi.arseniy@gmail.com>
2026-09-08 21:25:58 +0300

    init: start task

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	setlocale(LC_ALL, "Rus");
+//	puts("оПНЦПЮЛЛЮ ПЮАНРЮЕР!\n опнцпюллхпнбюмхе юкцнпхрлнб кхмеимни ярпсйрспш мю ъгшйе ях. янгдюмхе йнмянкэмшу опхкнфемхи я опхлемемхел йнлохкърнпю DEV-CP\n йПСРНИ ю. х. йт-17 6");
+	
+	int num;
+	num = 1024;
+	float PI = 3.1415927;
+	printf(" \n		Value pi=%f	\n %d - is 2^10 ", PI, num);
+	
+	getch();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
+
+
