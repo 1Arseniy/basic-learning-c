90bd7948e177436e8352abb99ef01877c4c6239a
1Arseniy <154349409+1Arseniy@users.noreply.github.com>
2026-09-09 22:47:37 +0300

    feat: create main-2.cpp

---
+++ main-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+
+int main(int argc, char** argv) {
+
+setlocale(LC_ALL, "Rus");
+
+
+int num;
+num = 1024;
+float PI = 3.1415927;
+printf(" \n		Value pi=%f	\n %d - is 2^10 ", PI, num);
+	
+getch();
+
+
+
+}
